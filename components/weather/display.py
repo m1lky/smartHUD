e20214df170bf89db0a13ede569a12e10d966a3f
james <jhebert@acis.com>
2019-12-07 16:10:48 -0500

    lighter blue

--- components/weather/display.py
+++ components/weather/display.py
@@ -12,7 +12,7 @@ class DisplayWeather(tk.Frame):
     self.api = api(latitude=42.401458, longitude=-71.126482)
     self.api.get_weather()
     self.place_data(self.api.get_display_data()) 
-    self.after(1000, self.update_data())
+    self.after(900 * 1000, self.update_data())
 
   def place_data(self, data):
     self._set_icon(data.get('icon'))
@@ -21,7 +21,7 @@ class DisplayWeather(tk.Frame):
     for row, key in enumerate(data):
       if key != 'icon':
         textvariable = tk.StringVar(value=key + ' is ' + str(data[key]))
-        message = tk.Label(self.root, textvariable=textvariable, width=50, background="black", fg="cyan", font=('Monospace', '25'), pady=5)
+        message = tk.Label(self.root, textvariable=textvariable, width=50, background="black", fg="#47daff", font=('Monospace', '25'), pady=5)
         self.message_boxes.append( (textvariable, message, key) )
         message.grid(row=(row % 2) + row + 2, column=1)
   
@@ -36,5 +36,5 @@ class DisplayWeather(tk.Frame):
       sleep(900) # 15 minutes
   def _set_icon(self, icon):
     self.image_tk = get_image_file(icon)
-    self.label_img = tk.Label(self.root, image=self.image_tk, background="black", fg="cyan")
+    self.label_img = tk.Label(self.root, image=self.image_tk, background="black", fg="#47daff")
     self.label_img.grid(row=1, column=1)
